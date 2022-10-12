bb625f08fafd0304fc8a99892029ee5341a7d50f
Nikhil Sengupta <nikhil.sengupta10@protonmail.com>
2022-10-12 13:27:09 +0100

    Fixed printing

--- MiniJava - ANTLR/src/minijava/symboltable/SymbolTable.java
+++ MiniJava - ANTLR/src/minijava/symboltable/SymbolTable.java
@@ -37,19 +37,19 @@ public class SymbolTable {
 		for (Map.Entry<String, ClassRecord> c : allClasses.entrySet()) {
 			Map<String, MethodRecord> allMethods = c.getValue().getMethods();
 			Map<String, VarRecord> allGlobalVars = c.getValue().getGlobalVars();
-			System.out.println("LEVEL 1: ClASS  " +  "TYPE: " + c.getValue().getType() + " IDENTITY: " + c.getValue().getId());
+			System.out.println("LEVEL 1: ClASS  " +  "TYPE: " + c.getValue().getType() + "\t IDENTITY: " + c.getValue().getId());
 			for (Map.Entry<String,VarRecord> g : allGlobalVars.entrySet()) {
-				System.out.println("		LEVEL 2: GLOBAL VARIABLE  " + "TYPE: " + g.getValue().getType() +  " IDENTITY: " + g.getValue().getId());
+				System.out.println("|		 LEVEL 2: GLOBAL VARIABLE  " + "TYPE: " + g.getValue().getType() +  "\t IDENTITY: " + g.getValue().getId());
 			}
 			for (Map.Entry<String,MethodRecord> m : allMethods.entrySet()) {
 				Map<String, VarRecord> allParams = m.getValue().getParameters();
 				Map<String, VarRecord> allLocalVars = m.getValue().getLocalVars();
-				System.out.println("		LEVEL 2: METHOD           " + "TYPE: " + m.getValue().getType() + " IDENTITY: " + m.getValue().getId());
+				System.out.println("|		 LEVEL 2: METHOD           " + "TYPE: " + m.getValue().getType() + "\t IDENTITY: " + m.getValue().getId());
 				for (Map.Entry<String, VarRecord> p : allParams.entrySet()) {
-					System.out.println("				LEVEL 3 PARAMETER:       " + "TYPE: " + p.getValue().getType() + " IDENTITY : " + p.getValue().getId());
+					System.out.println("|	 	 |		LEVEL 3 PARAMETER:       " + "TYPE: " + p.getValue().getType() + "\t IDENTITY : " + p.getValue().getId());
 				}
 				for (Map.Entry<String, VarRecord> l : allLocalVars.entrySet()) {
-					System.out.println("				LEVEL 3 LOCAL VARIABLE:  " + "TYPE: " + l.getValue().getType() +  " IDENTITY : " + l.getValue().getId());
+					System.out.println("|	 	 |		LEVEL 3 LOCAL VARIABLE:  " + "TYPE: " + l.getValue().getType() +  "\t IDENTITY : " + l.getValue().getId());
 				}
 			}
 		}
